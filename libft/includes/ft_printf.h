be80c13e2ac805ac21fda09c779080f453db5f07
tmatis <tmatis@student.42.fr>
2023-04-25 19:43:11 +0200

    ➕ add libft to git repo

---
+++ libft/includes/ft_printf.h
@@ -0,0 +1,11 @@
+#ifndef FT_PRINTF_H
+#define FT_PRINTF_H
+
+#include <stdlib.h>
+
+int ft_printf(const char *format, ...);
+int ft_dprintf(int fd, const char *format, ...);
+int ft_snprintf(char *dest, size_t size, const char *format, ...);
+int ft_asprintf(char **dest, const char *format, ...);
+
+#endif
